eee8dbce40e637ebb6ac91a8834ec6b5b08bc574
Jaime Santana <sistemas@santana.ec>
2022-03-14 23:22:08 -0500

    starter repo vue 3

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,78 @@
+<script setup>
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+import ToolingIcon from "./icons/IconTooling.vue";
+import EcosystemIcon from "./icons/IconEcosystem.vue";
+import CommunityIcon from "./icons/IconCommunity.vue";
+import SupportIcon from "./icons/IconSupport.vue";
+</script>
+
+<template>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Clubs</template>
+
+    Get official tools and libraries for your project:
+
+    a visit.
+  </WelcomeItem>
+
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Series</template>
+
+    Got stuck? Ask your question on
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Jugadores</template>
+
+    De cada jugador se solicita 
+  </WelcomeItem>
+
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Torneos</template>
+
+    Club, horario, ubicación, organizador
+    <br />
+
+    More instructions are available in <code>README.md</code>.
+  </WelcomeItem>
+
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Grupos</template>
+
+    Got stuck? Ask your question on
+    
+  </WelcomeItem>
+
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Partidos</template>
+
+    Got stuck? Ask your question on
+
+  </WelcomeItem>
+
+</template>
