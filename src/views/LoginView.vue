8b6b23cb3df88e0c83bd2f760cbab2316b186061
Jaime Santana <sistemas@santana.ec>
2022-03-16 23:57:54 -0500

    forms mejorados

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,6 +7,7 @@ export default {
       form: {
         email: "",
         password: "",
+        recordarme: false
       },
       error: null,
     };
@@ -53,7 +54,7 @@ export default {
         >
           <div class="rounded-t mb-0 px-6 py-6">
             <div class="text-center mb-3">
-              <h6 class="text-gray-600 text-sm font-bold">Sign in with</h6>
+              <h6 class="text-gray-600 text-sm font-bold">Iniciar sesión con </h6>
             </div>
             <div class="btn-wrapper text-center">
               <button
@@ -120,16 +121,17 @@ export default {
           </div>
           <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
             <div class="text-gray-500 text-center mb-3 font-bold">
-              <small>Or sign in with credentials</small>
+              <small>O inicia con tus credenciales</small>
             </div>
             <form @submit.prevent="submit">
               <div class="relative w-full mb-3">
                 <label
                   class="block uppercase text-gray-700 text-xs font-bold mb-2"
                   for="grid-password"
-                  >Email</label
+                  >Correo electrónico</label
                 ><input
                   type="email"
+                  v-model="form.email"
                   class="
                     border-0
                     px-3
@@ -143,7 +145,7 @@ export default {
                     focus:outline-none focus:ring
                     w-full
                   "
-                  placeholder="Email"
+                  placeholder="Correo electrónico"
                   style="transition: all 0.15s ease 0s"
                 />
               </div>
@@ -151,9 +153,10 @@ export default {
                 <label
                   class="block uppercase text-gray-700 text-xs font-bold mb-2"
                   for="grid-password"
-                  >Password</label
+                  >Contraseña</label
                 ><input
                   type="password"
+                  v-model="form.password"
                   class="
                     border-0
                     px-3
@@ -167,7 +170,7 @@ export default {
                     focus:outline-none focus:ring
                     w-full
                   "
-                  placeholder="Password"
+                  placeholder="Contraseña"
                   style="transition: all 0.15s ease 0s"
                 />
               </div>
@@ -176,6 +179,7 @@ export default {
                   ><input
                     id="customCheckLogin"
                     type="checkbox"
+                    v-model="form.recordarme"
                     class="
                       form-checkbox
                       border-0
@@ -187,7 +191,7 @@ export default {
                     "
                     style="transition: all 0.15s ease 0s"
                   /><span class="ml-2 text-sm font-semibold text-gray-700"
-                    >Remember me</span
+                    >Recordarme</span
                   ></label
                 >
               </div>
@@ -214,22 +218,22 @@ export default {
                   type="button"
                   style="transition: all 0.15s ease 0s"
                 >
-                  Sign In
+                  Iniciar sesión
                 </button>
+                <div class="flex justify-around flex-wrap bg-black mt-6">
+                  <div class="w-1/2">
+                    <a href="#pablo" class="text-gray-300"
+                      ><small>Olvidaste tu Contraseña?</small></a
+                    >
+                  </div>
+                  <div class="w-1/2">
+                    <RouterLink to="/register" class="text-gray-300">
+                      Crear nueva cuenta
+                    </RouterLink>
+                  </div>
+                </div>
               </div>
             </form>
-          </div>
-        </div>
-        <div class="flex flex-wrap mt-6">
-          <div class="w-1/2">
-            <a href="#pablo" class="text-gray-300"
-              ><small>Forgot password?</small></a
-            >
-          </div>
-          <div class="w-1/2 text-right">
-            <a href="#pablo" class="text-gray-300"
-              ><small>Create new account</small></a
-            >
           </div>
         </div>
       </div>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,6 +1,6 @@
 <script>
 
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, updateProfile, createUserWithEmailAndPassword } from "firebase/auth";
 
 
 export default {
@@ -23,11 +23,16 @@ export default {
           const user = data.user;
           console.log('registrado');
           console.log('data',data);
-          data.user
-            .updateProfile({
-              displayName: this.form.name
+          updateProfile(user,{
+              displayName: this.form.name,
+              variable: 'vasd'
             })
-            .then(() => {});
+            .then(() => {
+              this.$router.replace({ name: "dashboard" });
+            }).catch((error) => {
+              console.log('fallo profile');
+              console.log(error);
+            });
           // ...
         })
         .catch((error) => {
@@ -45,76 +50,208 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">Register</div>
-          <div class="card-body">
-            <div v-if="error" class="alert alert-danger">{{ error }}</div>
-            <form action="#" @submit.prevent="submit">
-              <div class="form-group row">
-                <label for="name" class="col-md-4 col-form-label text-md-right"
-                  >Name</label
-                >
-
-                <div class="col-md-6">
-                  <input
-                    id="name"
-                    type="name"
-                    class="form-control"
-                    name="name"
-                    value
-                    required
-                    autofocus
-                    v-model="form.name"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right"
-                  >Email</label
-                >
-
-                <div class="col-md-6">
-                  <input
-                    id="email"
-                    type="email"
-                    class="form-control"
-                    name="email"
-                    value
-                    required
-                    autofocus
-                    v-model="form.email"
-                  />
-                </div>
-              </div>
-
-              <div class="form-group row">
+  <div class="container mx-auto px-4 h-full">
+    <div class="flex content-center items-center justify-center h-full">
+      <div class="w-full px-4">
+        <div
+          class="
+            relative
+            flex flex-col
+            min-w-0
+            break-words
+            w-full
+            mb-6
+            shadow-lg
+            rounded-lg
+            bg-gray-300
+            border-0
+          "
+        >
+          <div class="rounded-t mb-0 px-6 py-6">
+            <div class="text-center mb-3">
+              <h6 class="text-gray-600 text-sm font-bold">Registrate con </h6>
+            </div>
+            <div class="btn-wrapper text-center">
+              <button
+                class="
+                  bg-white
+                  active:bg-gray-100
+                  text-gray-800
+                  font-normal
+                  px-4
+                  py-2
+                  rounded
+                  outline-none
+                  focus:outline-none
+                  mr-2
+                  mb-1
+                  uppercase
+                  shadow
+                  hover:shadow-md
+                  inline-flex
+                  items-center
+                  font-bold
+                  text-xs
+                "
+                type="button"
+                style="transition: all 0.15s ease 0s"
+              >
+                <img
+                  alt="..."
+                  class="w-5 mr-1"
+                  src="../assets/img/github.svg"
+                />Github</button
+              ><button
+                class="
+                  bg-white
+                  active:bg-gray-100
+                  text-gray-800
+                  font-normal
+                  px-4
+                  py-2
+                  rounded
+                  outline-none
+                  focus:outline-none
+                  mr-1
+                  mb-1
+                  uppercase
+                  shadow
+                  hover:shadow-md
+                  inline-flex
+                  items-center
+                  font-bold
+                  text-xs
+                "
+                type="button"
+                style="transition: all 0.15s ease 0s"
+              >
+                <img
+                  alt="..."
+                  class="w-5 mr-1"
+                  src="../assets/img/google.svg"
+                />Google
+              </button>
+            </div>
+            <hr class="mt-6 border-b-1 border-gray-400" />
+          </div>
+          <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+            <div class="text-gray-500 text-center mb-3 font-bold">
+              <small>O registrate con tus credenciales</small>
+            </div>
+            <form @submit.prevent="submit">
+              <div class="relative w-full mb-3">
                 <label
-                  for="password"
-                  class="col-md-4 col-form-label text-md-right"
-                  >Password</label
-                >
-
-                <div class="col-md-6">
-                  <input
-                    id="password"
-                    type="password"
-                    class="form-control"
-                    name="password"
-                    required
-                    v-model="form.password"
-                  />
-                </div>
+                  class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                  for="grid-password"
+                  >Nombre</label
+                ><input
+                  type="text"
+                  v-model="form.name"
+                  class="
+                    border-0
+                    px-3
+                    py-3
+                    placeholder-gray-400
+                    text-gray-700
+                    bg-white
+                    rounded
+                    text-sm
+                    shadow
+                    focus:outline-none focus:ring
+                    w-full
+                  "
+                  placeholder="Nombre"
+                  style="transition: all 0.15s ease 0s"
+                  required
+                />
               </div>
+              <div class="relative w-full mb-3">
+                <label
+                  class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                  for="grid-password"
+                  >Correo electrónico</label
+                ><input
+                  type="email"
+                  v-model="form.email"
+                  class="
+                    border-0
+                    px-3
+                    py-3
+                    placeholder-gray-400
+                    text-gray-700
+                    bg-white
+                    rounded
+                    text-sm
+                    shadow
+                    focus:outline-none focus:ring
+                    w-full
+                  "
+                  placeholder="Correo electrónico"
+                  style="transition: all 0.15s ease 0s"
+                  required
+                />
+              </div>
+              <div class="relative w-full mb-3">
+                <label
+                  class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                  for="grid-password"
+                  >Contraseña</label
+                ><input
+                  type="password"
+                  v-model="form.password"
+                  class="
+                    border-0
+                    px-3
+                    py-3
+                    placeholder-gray-400
+                    text-gray-700
+                    bg-white
+                    rounded
+                    text-sm
+                    shadow
+                    focus:outline-none focus:ring
+                    w-full
+                  "
+                  placeholder="Contraseña"
+                  style="transition: all 0.15s ease 0s"
+                  required
+                />
+              </div>
+              <div class="text-center mt-6">
+                <button
+                  class="
+                    bg-gray-900
+                    text-white
+                    active:bg-gray-700
+                    text-sm
+                    font-bold
+                    uppercase
+                    px-6
+                    py-3
+                    rounded
+                    shadow
+                    hover:shadow-lg
+                    outline-none
+                    focus:outline-none
+                    mr-1
+                    mb-1
+                    w-full
+                  "
+                  type="submit"
+                  style="transition: all 0.15s ease 0s"
+                >
+                  Registrarme
+                </button>
+                <div class="flex justify-around flex-wrap bg-black mt-6">
+                  <div class="w-1/2">
+                   Ya tienes una cuenta?
+                  </div>
+                  <div class="w-1/2 ">
+                    <RouterLink to="/login" class="text-gray-300">
+                      Iniciar sesión
+                    </RouterLink>
 
-              <div class="form-group row mb-0">
-                <div class="col-md-8 offset-md-4">
-                  <button type="submit" class="btn btn-primary">
-                    Register
-                  </button>
+                  </div>
                 </div>
               </div>
             </form>
