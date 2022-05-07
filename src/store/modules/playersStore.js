e7dbb4452553ab91cc2bfdf3b0120b869b2d28f1
Jaime Santana <sistemas@santana.ec>
2022-05-06 19:06:42 -0500

    fix pagi nado

--- src/store/modules/playersStore.js
+++ src/store/modules/playersStore.js
@@ -1,215 +1,233 @@
 import { db } from '../../plugins/firebase';
 
 import {
-  getFirestore,
-  collection,
-  onSnapshot,
-  query,
-  where,
-  doc,
-  getDocs,
-  getDoc,
-  addDoc,
-  setDoc,
+	getFirestore,
+	collection,
+	onSnapshot,
+	query,
+	where,
+	doc,
+	getDocs,
+	orderBy,
+	getDoc,
+	addDoc,
+	setDoc,
 } from "firebase/firestore";
 
 const state = {
-  players: [],
-  playersListener: () => {},
-  error: null,
+	players: [],
+	playersListener: () => { },
+	error: null,
 };
 
 const getters = {
-  getPlayers(state) {
-    return state.plsyers;
-  },
-  getPlayer(state, id) {
-    return state.players.find((player) => player.id === id);
-  },
+	getPlayers(state) {
+		return state.plsyers;
+	},
+	getPlayer(state, id) {
+		return state.players.find((player) => player.id === id);
+	},
 };
 
 const actions = {
-  async loadPlayers({ commit }, payload) {
-    console.log("loadPlayers");
-    console.log(db);
-    const querySnapshot = await getDocs(collection(db, "players"));
+	async loadPlayers({ commit }, payload) {
+		console.log("loadPlayers");
 
-    console.log(querySnapshot);
-    let players = [];
-    doSnapShot(querySnapshot);
+		if (payload.lastPlayer) {
+			const docRef = doc(db, "players", payload.lastPlayer.id);
+			const docSnap = await getDoc(docRef);
+			q = query(collection(db, "players"),
+				orderBy('ranking', "desc"),
+				startAfter(docSnap),
+				limit(payload.limit)
+			)
+		} else {
+			commit("SET_PLAYERS", []);
+			q = query(collection(db, "players"),
+				orderBy('ranking', "desc"),
+				limit(payload.limit)
+			)
+		}
 
-    //commit("SET_PLAYERS_LISTENER", query);
+		const querySnapshot = await getDocs(q);
 
-    function doSnapShot(querySnapshot) {
-      console.log("doSnapShot");
-      console.log(querySnapshot);
-      console.log(querySnapshot.docs);
-      querySnapshot.docs.forEach((doc) => {
-        let p = doc.data();
-        console.log(`${doc.id} => ${doc.data()}`);
-        p.id = doc.id;
-        players.push(p);
-      });
-      commit("SET_PLAYERS", players);
-    }
-  },
-  async fetchPlayersOptions({ commit }, payload) {
-    console.log("loadPlayersOptions"); 
-    let q;
-    if(payload.idsInscritos.length > 0){
-      q = query(collection(db, "players"), where("uid", "not-in", payload.idsInscritos));
-    }else{
-      q = query(collection(db, "players"));
-    }
+		console.log(querySnapshot);
+		let players = [];
+		return doSnapShot(querySnapshot);
 
-    const queryPlayersSnapshot = await getDocs(q);
-    
-      console.log(queryPlayersSnapshot);
-      let players = [];
-      return doSnapShot(queryPlayersSnapshot);
+		//commit("SET_PLAYERS_LISTENER", query);
 
-      function doSnapShot(queryPlayersSnapshot) {
-        console.log("doSnapShot");
-        console.log(queryPlayersSnapshot.docs);
-        queryPlayersSnapshot.docs.forEach((doc) => {
-          let p = doc.data();
-          console.log(`Player: ${doc.id} => ${doc.data()}`);
-          p.id = doc.id;
-          players.push(p);
-        });
-        return players;
-      }
-  },
-  async loadProfileByUid({ commit }, payload) {
-    try {
-        console.log('%ctorneosStore.js line:69 payload', 'color: #007acc;', payload);
-        let q = query(collection(db, "players"), where("uid", "==", payload));
-        const docSnap = await getDocs(q);
+		function doSnapShot(querySnapshot) {
+			console.log("doSnapShotPlayers");
+			console.log(querySnapshot.docs);
+			querySnapshot.docs.forEach((doc) => {
+				let p = doc.data();
+				console.log(`Player: ${doc.id} => ${doc.data()}`);
+				p.id = doc.id;
+				players.push(p);
+				commit("ADD_PLAYER", p);
+			});
+			//commit("SET_PLAYERS", players);
+			return players;
+		}
+	},
+	async fetchPlayersOptions({ commit }, payload) {
+		console.log("loadPlayersOptions");
+		let q;
+		if (payload.idsInscritos.length > 0) {
+			q = query(collection(db, "players"), where("uid", "not-in", payload.idsInscritos));
+		} else {
+			q = query(collection(db, "players"));
+		}
 
-      if (docSnap.docs) {
-          
-            console.log("Profile UID data:", docSnap.docs[0].data());
-            return docSnap.docs[0].data();
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-            return null;
-        }
-    } catch (e) {
-        console.error("Error adding document: ", e);
-    }
-  },
-  async loadProfile({ commit }, payload) {
-    try {
-        console.log('%ctorneosStore.js line:69 payload', 'color: #007acc;', payload);
-        const docRef = doc(db, "players", payload);
-        const docSnap = await getDoc(docRef);
+		const queryPlayersSnapshot = await getDocs(q);
 
-        if (docSnap.exists()) {
-            console.log("Profile data:", docSnap.data());
-            return docSnap.data();
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-            return null;
-        }
-    } catch (e) {
-        console.error("Error adding document: ", e);
-    }
-  },
-  addPlayer({ commit }, payload) {
-    const db = getFirestore();
-    try {
-      addDoc(collection(db, "players"), {
-        nombre: payload.user.displayName ?? "",
-        bod: null,
-        nacionalidad: "ec",
-        sexo: "H",
-        avatar: payload.user.photoURL ?? null,
-      })
-        .then((docRef) => {
-          console.log("Document written with ID: ", docRef);
-        })
-        .catch((error) => {
-          console.log("error init Player");
-          console.log(error);
-        });
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  },
-  async updatePlayer({ commit }, payload) {
-    const db = getFirestore();
-    try {
-      return await setDoc(doc(db, "players", payload.user.uid), {
-        nombre: payload.user.displayName ?? "",
-        bod: null,
-        nacionalidad: "ec",
-        sexo: "H",
-        avatar: payload.user.photoURL ?? null,
-      })
-        .then((docRef) => {
-          console.log("Document written with ID: ", docRef);
-          return docRef;
-        })
-        .catch((error) => {
-          console.log("error init Player");
-          console.log(error);
-          return null;
-        });
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  },
-  async importPlayers({ commit }, payload) {
+		console.log(queryPlayersSnapshot);
+		let players = [];
+		return doSnapShot(queryPlayersSnapshot);
 
-    console.log('payloadlll',payload);
+		function doSnapShot(queryPlayersSnapshot) {
+			console.log("doSnapShot");
+			console.log(queryPlayersSnapshot.docs);
+			queryPlayersSnapshot.docs.forEach((doc) => {
+				let p = doc.data();
+				console.log(`Player: ${doc.id} => ${doc.data()}`);
+				p.id = doc.id;
+				players.push(p);
+			});
+			return players;
+		}
+	},
+	async loadProfileByUid({ commit }, payload) {
+		try {
+			console.log('%ctorneosStore.js line:69 payload', 'color: #007acc;', payload);
+			let q = query(collection(db, "players"), where("uid", "==", payload));
+			const docSnap = await getDocs(q);
 
-    payload.forEach(p => {
-      console.log('p',p);
-      addDoc(collection(db, "players"), {
-        nombre: p[2],
-        bod: null,
-        nacionalidad: "ec",
-        sexo: "H",
-        avatar: null,
-        total_torneos:0,
-        total_partidos: 0,
-        total_victorias: 0,
-        total_derrotas: 0,
-        ranking: p[0],
-        puntos: p[5],
-        n_socio: p[3]
-      })
-        .then((docRef) => {
-          console.log("Document written with ID: ", docRef);
-          return docRef;
-        })
-    })
-  }
+			if (docSnap.docs) {
+
+				console.log("Profile UID data:", docSnap.docs[0].data());
+				return docSnap.docs[0].data();
+			} else {
+				// doc.data() will be undefined in this case
+				console.log("No such document!");
+				return null;
+			}
+		} catch (e) {
+			console.error("Error adding document: ", e);
+		}
+	},
+	async loadProfile({ commit }, payload) {
+		try {
+			console.log('%ctorneosStore.js line:69 payload', 'color: #007acc;', payload);
+			const docRef = doc(db, "players", payload);
+			const docSnap = await getDoc(docRef);
+
+			if (docSnap.exists()) {
+				console.log("Profile data:", docSnap.data());
+				return docSnap.data();
+			} else {
+				// doc.data() will be undefined in this case
+				console.log("No such document!");
+				return null;
+			}
+		} catch (e) {
+			console.error("Error adding document: ", e);
+		}
+	},
+	addPlayer({ commit }, payload) {
+		const db = getFirestore();
+		try {
+			addDoc(collection(db, "players"), {
+				nombre: payload.user.displayName ?? "",
+				bod: null,
+				nacionalidad: "ec",
+				sexo: "H",
+				avatar: payload.user.photoURL ?? null,
+			})
+				.then((docRef) => {
+					console.log("Document written with ID: ", docRef);
+				})
+				.catch((error) => {
+					console.log("error init Player");
+					console.log(error);
+				});
+		} catch (e) {
+			console.error("Error adding document: ", e);
+		}
+	},
+	async updatePlayer({ commit }, payload) {
+		const db = getFirestore();
+		try {
+			return await setDoc(doc(db, "players", payload.user.uid), {
+				nombre: payload.user.displayName ?? "",
+				bod: null,
+				nacionalidad: "ec",
+				sexo: "H",
+				avatar: payload.user.photoURL ?? null,
+			})
+				.then((docRef) => {
+					console.log("Document written with ID: ", docRef);
+					return docRef;
+				})
+				.catch((error) => {
+					console.log("error init Player");
+					console.log(error);
+					return null;
+				});
+		} catch (e) {
+			console.error("Error adding document: ", e);
+		}
+	},
+	async importPlayers({ commit }, payload) {
+
+		console.log('payloadlll', payload);
+
+		payload.forEach(p => {
+			console.log('p', p);
+			addDoc(collection(db, "players"), {
+				nombre: p[2],
+				bod: null,
+				nacionalidad: "ec",
+				sexo: "H",
+				avatar: null,
+				total_torneos: 0,
+				total_partidos: 0,
+				total_victorias: 0,
+				total_derrotas: 0,
+				ranking: p[0],
+				puntos: p[5],
+				n_socio: p[3]
+			})
+				.then((docRef) => {
+					console.log("Document written with ID: ", docRef);
+					return docRef;
+				})
+		})
+	}
 };
 const mutations = {
-  SET_PLAYERS_LISTENER(state, listener) {
-    if (listener) {
-      state.playersListener = listener;
-    } else {
-      state.playersListener();
-    }
-  },
-  SET_PLAYERS(state, payload) {
-    console.log("setPlayers", payload);
-    state.players = payload;
-  },
-  ADD_PLAYER(state, payload) {
-    state.players.push(payload);
-  },
-  setError(state, payload) {
-    state.error = payload;
-  },
+	SET_PLAYERS_LISTENER(state, listener) {
+		if (listener) {
+			state.playersListener = listener;
+		} else {
+			state.playersListener();
+		}
+	},
+	SET_PLAYERS(state, payload) {
+		console.log("setPlayers", payload);
+		state.players = payload;
+	},
+	ADD_PLAYER(state, payload) {
+		state.players.push(payload);
+	},
+	setError(state, payload) {
+		state.error = payload;
+	},
 };
 export default {
-  state,
-  getters,
-  actions,
-  mutations,
+	state,
+	getters,
+	actions,
+	mutations,
 };
--- src/views/game/players/PlayersList.vue
+++ src/views/game/players/PlayersList.vue
@@ -16,7 +16,7 @@ export default {
       BreadcrumbTitle: "Jugadores",
       BreadcrumbSubTitle: "Listado",
       players: [],
-      maxPerPage: 3,
+      maxPerPage: 12,
       showReadMore: true,
       procesandoShowMore: false
     };
@@ -24,7 +24,7 @@ export default {
   methods: {
     ...mapActions(["loadPlayers"]),
     loadMore() {
-        let lastTorneo = this.matchesData[this.matchesData.length-1];
+        let lastTorneo = this.players[this.players.length-1];
         let resultados = this.loadTorneos({
             limit: this.maxPerPage,
             lastTorneo: lastTorneo
@@ -38,7 +38,9 @@ export default {
     },
   },
   mounted() {
-    this.loadPlayers();
+    this.loadPlayers({
+            limit: this.maxPerPage
+    });
     
   },
 
